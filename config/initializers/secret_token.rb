7f5513b903813302edf44d359a65615544ae80e5
GroundskeeperWillie <portlyowl@gmail.com>
2013-02-15 20:10:22 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Powd1::Application.config.secret_token = '58aefb5428ab433d46fda27b2e429fcb08dd41f5b882ec415c83bbcaa9409de1eb78f3bcd00fcb03ed3c2b921b5e88489191954ac47e50b455564802c2390ddd'
